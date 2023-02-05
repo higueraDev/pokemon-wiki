b77de531032847c0f4c5f738225b7e0d0e41c12f
higueraDev <49871959+higueraDev@users.noreply.github.com>
2023-02-04 19:01:56 -0700

    [UPDATED]: firestore service CRUD

--- src/app/core/firestore.service.ts
+++ src/app/core/firestore.service.ts
@@ -1,13 +1,59 @@
 import { Injectable } from '@angular/core';
-import { CollectionReference, DocumentData, Firestore } from '@angular/fire/firestore';
+import {
+  collection,
+  collectionData,
+  CollectionReference,
+  docData,
+  DocumentData,
+  Firestore,
+} from '@angular/fire/firestore';
+import { addDoc, deleteDoc, doc, DocumentReference, updateDoc } from '@firebase/firestore';
+import { Pokemon } from '../models/entities/pokemon';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FirestoreService {
+  private pokemonCollection: CollectionReference<Pokemon>;
 
-  private pokemonCollection: CollectionReference<DocumentData>
+  constructor(private readonly db: Firestore) {
+    this.pokemonCollection = collection(
+      this.db,
+      'pokemon'
+    ) as CollectionReference<Pokemon>;
+  }
 
-  constructor(private readonly db: Firestore) { }
+  getAll() {
+    return collectionData(this.pokemonCollection, {
+      idField: 'id',
+    });
+  }
 
+  get(id: Pokemon['id']) {
+    const pokemonDocumentReference = doc(
+      this.db,
+      `pokemon/${id}`
+    ) as DocumentReference<Pokemon>;
+    return docData<Pokemon>(pokemonDocumentReference, { idField: 'id' });
+  }
+
+  create(pokemon: Pokemon) {
+    return addDoc(this.pokemonCollection, pokemon);
+  }
+
+  update(pokemon: Pokemon) {
+    const pokemonDocumentReference = doc(
+      this.db,
+      `pokemon/${pokemon.id}`
+    ) as DocumentReference<Pokemon>;
+    return updateDoc<Pokemon>(pokemonDocumentReference, { ...pokemon });
+  }
+
+  delete(id: Pokemon['id']) {
+    const pokemonDocumentReference = doc(
+      this.db,
+      `pokemon/${id}`
+    )
+    return deleteDoc(pokemonDocumentReference)
+  }
 }
