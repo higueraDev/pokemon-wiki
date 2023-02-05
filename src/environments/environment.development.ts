770352d9e2faf786f8544c7dc603782ba5f3cc48
higueraDev <49871959+higueraDev@users.noreply.github.com>
2023-02-04 18:19:06 -0700

    [ADDED]: firestore

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'pokemon-wiki-7577e',
+    appId: '1:777426924497:web:6ef9e8e3bbd921f633e6f0',
+    storageBucket: 'pokemon-wiki-7577e.appspot.com',
+    apiKey: 'AIzaSyD7tDXVouXGTj1FiINYuhHaqPPjdm9PMFQ',
+    authDomain: 'pokemon-wiki-7577e.firebaseapp.com',
+    messagingSenderId: '777426924497',
+  },};
